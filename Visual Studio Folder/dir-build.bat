d73aea7bf3e795664d1095bf26646a3fee8e96c8
Kayne Ruse <ratstail91@yahoo.com.au>
2012-05-09 13:21:36 +1000

    Created dir-build.bat to automate a larger post-build event.

---
+++ Visual Studio Folder/dir-build.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+REM I don't know how portible this is, but it was written on Windows 7
+
+REM This batch file builds the library directory from *.h and *.cpp, which is a convenience, not a necessity
+REM Simply call this script; the Visual Studio solution calls this after the library is built
+
+REM parameter list:
+REM %0 $(SolutionDir)dir-build
+
+xcopy "%~p0Codebase Library\*.h" "%~p0Release\Codebase Library\include\" /D /S /Y /I
+xcopy "%~p0Quick Start\*.h" "%~p0Release\Codebase Library\Quick Start\" /D /S /Y /I
+xcopy "%~p0Quick Start\*.cpp" "%~p0Release\Codebase Library\Quick Start\" /D /S /Y /I
