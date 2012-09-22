c76688d5a82f245c17357820f14eba079530e22c
Kayne Ruse <ratstail91@yahoo.com.au>
2012-09-23 09:52:37 +1000

    Removed zlib license from the generated  text, since not everybody will want to use that copyright

--- Visual Studio Folder/Scene Framework/scene generator.lua
+++ Visual Studio Folder/Scene Framework/scene generator.lua
@@ -56,29 +56,8 @@ hFile:write(
 "/* File Name: " .. fname .. ".h\
  * Author: " .. author .. "\
  * Date (dd/mm/yyyy): " .. os.date("%d/%m/%Y") .. "\
- * Copyright: (c) " .. author .. " " .. os.date("%Y") .. "\
- *\
- * This software is provided 'as-is', without any express or implied\
- * warranty. In no event will the authors be held liable for any damages\
- * arising from the use of this software.\
- *\
- * Permission is granted to anyone to use this software for any purpose,\
- * including commercial applications, and to alter it and redistribute it\
- * freely, subject to the following restrictions:\
- *\
- * 1. The origin of this software must not be misrepresented; you must not\
- * claim that you wrote the original software. If you use this software\
- * in a product, an acknowledgment in the product documentation would be\
- * appreciated but is not required.\
- *\
- * 2. Altered source versions must be plainly marked as such, and must not be\
- * misrepresented as being the original software.\
- *\
- * 3. This notice may not be removed or altered from any source\
- * distribution.\
- *\
- * Description:\
- *     ...\
+ * Copyright: ...\
+ * Description: ...\
 */\
 #ifndef " .. guard .. "\
 #define " .. guard .. "\
@@ -122,29 +101,8 @@ hFile:write(
 "/* File Name: " .. fname .. ".cpp\
  * Author: " .. author .. "\
  * Date (dd/mm/yyyy): " .. os.date("%d/%m/%Y") .. "\
- * Copyright: (c) " .. author .. " " .. os.date("%Y") .. "\
- *\
- * This software is provided 'as-is', without any express or implied\
- * warranty. In no event will the authors be held liable for any damages\
- * arising from the use of this software.\
- *\
- * Permission is granted to anyone to use this software for any purpose,\
- * including commercial applications, and to alter it and redistribute it\
- * freely, subject to the following restrictions:\
- *\
- * 1. The origin of this software must not be misrepresented; you must not\
- * claim that you wrote the original software. If you use this software\
- * in a product, an acknowledgment in the product documentation would be\
- * appreciated but is not required.\
- *\
- * 2. Altered source versions must be plainly marked as such, and must not be\
- * misrepresented as being the original software.\
- *\
- * 3. This notice may not be removed or altered from any source\
- * distribution.\
- *\
- * Description:\
- *     ...\
+ * Copyright: ...\
+ * Description: ...\
 */\
 #include \"" .. fname .. ".h\"\
 \
