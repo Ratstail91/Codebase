9b8b02365893d2d4def616753bc3ee601c19415d
Kayne Ruse <ratstail91@yahoo.com.au>
2013-05-10 03:07:14 +1000

    Brought all systems except BBox up to the new standard

--- config_utility.hpp
+++ config_utility.hpp
@@ -1,52 +1,68 @@
-/* File Name: config_utility.h
- * Author: Kayne Ruse
- * Date (dd/mm/yyyy): 19/9/2012
- * Copyright: (c) Kayne Ruse 2012
- *
+/* Copyright: (c) Kayne Ruse 2013
+ * 
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
- *
+ * 
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- *
+ * 
  * 1. The origin of this software must not be misrepresented; you must not
  * claim that you wrote the original software. If you use this software
  * in a product, an acknowledgment in the product documentation would be
  * appreciated but is not required.
- *
+ * 
  * 2. Altered source versions must be plainly marked as such, and must not be
  * misrepresented as being the original software.
- *
+ * 
  * 3. This notice may not be removed or altered from any source
  * distribution.
- *
- * Description:
- *     Load and process configuration values from a file.
 */
-#ifndef KR_CONFIGUTILITY_H_
-#define KR_CONFIGUTILITY_H_ 2012091901
+#ifndef CONFIGUTILITY_HPP_
+#define CONFIGUTILITY_HPP_
 
 #include <map>
 #include <string>
+#include <cstdlib>
 
 class ConfigUtility {
 public:
-	/* Public access members */
+	ConfigUtility() {}
+	ConfigUtility(std::string s) { Load(s); }
+
 	void Load(std::string fname);
 
-	std::string operator[]	(std::string);
-	const char* CString		(std::string);
-	int Integer				(std::string);
-	double Double			(std::string);
-	bool Boolean			(std::string);
+	std::string String(std::string s) {
+		return table[s];
+	}
+	const char* CString(std::string s) {
+		return table[s].c_str();
+	}
+	int Integer(std::string s) {
+		return atoi(table[s].c_str());
+	}
+	double Double(std::string s) {
+		return atof(table[s].c_str());
+	}
+	bool Boolean(std::string s) {
+		return table[s] == "true";
+	}
 
-	std::map<std::string, std::string>* GetMap();
-
+	std::string& operator[](std::string s) {
+		return table[s];
+	}
+	int Int(std::string s) {
+		return Integer(s);
+	}
+	int Bool(std::string s) {
+		return Boolean(s);
+	}
+	std::map<std::string, std::string>* GetMap() {
+		return &table;
+	}
 private:
-	/* Private access members */
-	std::map<std::string, std::string> m_vTable;
+	std::map<std::string, std::string> table;
 };
 
 #endif
